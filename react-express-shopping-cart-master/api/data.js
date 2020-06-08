c25fdf8cdbc5608758107d3f508e3ebbcb431c6b
Gaurav Shukla <gauravshkla001@gmail.com>
2020-06-08 23:39:40 +0530

    latest cart

---
+++ react-express-shopping-cart-master/api/data.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    id: 01,
+    name: 'Product 1',
+    available_quantity: 5,
+    price: 450,
+    description: 'XYZ'
+  },
+
+  {
+    id: 02,
+    name: 'Product 2',
+    available_quantity: 7,
+    price: 50,
+    description: 'XYZ'
+  },
+
+  {
+    id: 03,
+    name: 'Product 3',
+    available_quantity: 0,
+    price: 500,
+    description: 'XYZ'
+  },
+
+  {
+    id: 04,
+    name: 'Product 4',
+    available_quantity: 4,
+    price: 1500,
+    description: 'XYZ'
+  },
+];
+
+
+
+const users = [
+    {
+      'name': 'user',
+      'password': 'qwerty'
+    },
+    {
+      'name': 'example',
+      'password': 'qwerty'
+    }
+];
+
+module.exports = { 'products': products, users: users }
